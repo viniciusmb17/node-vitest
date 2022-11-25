a18668408b1e608544879c60884cf2840233f83e
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-11-25 17:58:02 -0300

    first commit

---
+++ src/use-cases/create-appointment.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest'
+import { Appointment } from '../entities/appointment'
+import { getFutureDate } from '../tests/utils/get-future-date'
+import { CreateAppointment } from './create-appointment'
+
+describe('Create Appointment', () => {
+  it('should be able to create an appointment', () => {
+    const createAppointment = new CreateAppointment()
+
+    const startsAt = getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-11')
+
+    void expect(
+      createAppointment.execute({
+        customer: 'John Doe',
+        startsAt,
+        endsAt,
+      }),
+    ).resolves.toBeInstanceOf(Appointment)
+  })
+})
